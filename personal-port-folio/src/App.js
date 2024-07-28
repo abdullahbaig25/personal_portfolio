029d3fec7a32b915ca7563e94521689785ae286c
abdullahbaig25 <bsce20025@itu.edu.pk>
2024-07-28 18:55:28 +0500

    pushed

--- personal-port-folio/src/App.js
+++ personal-port-folio/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/Herosection";
+import About from "./components/About";
+import Skills from "./components/Skills";
 
-import HeroSection from './components/Herosection';
-import About from './components/About';
-import Services from './components/Skills';
+import "./App.css"
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HeroSection />
-      <About/>
-      <Services/>
+      <div id="hero">
+        <HeroSection />
+      </div>
+      <div id="about">
+        <About />
+      </div>
+      <div id="skills">
+        <Skills />
+      </div>
     </div>
   );
 }
--- personal-port-folio/src/components/Navbar.js
+++ personal-port-folio/src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 
 const menuItems = [
-  "Home",
-  "About",
-  "Skills",
-  "contact",
-  
+  { name: "Home", href: "#hero" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Contact", href: "#contact" },
 ];
 
 const Navbar = () => {
@@ -15,14 +14,14 @@ const Navbar = () => {
 
   const renderMenuItems = (isMobile = false) => (
     menuItems.map((item) => (
-      <li key={item} className={`px-3 py-2 ${isMobile ? "w-full" : ""} hover:bg-blue-600  hover:text-white  rounded-lg transition-all cursor-pointer`}>
-        <a href="#" className={isMobile ? "block" : "text-black hover:text-white"}>{item}</a>
+      <li key={item.name} className={`px-3 py-2 ${isMobile ? "w-full" : ""} hover:bg-blue-600 hover:text-white rounded-lg transition-all cursor-pointer`}>
+        <a href={item.href} className={isMobile ? "block" : "text-black hover:text-white"}>{item.name}</a>
       </li>
     ))
   );
 
   return (
-    <div className="w-full bg-white ">
+    <div className="w-full bg-white">
       <nav className="container mx-auto flex items-center justify-between gap-3 px-4 py-3">
         <div className="flex items-center gap-4">
           <span className="px-3 py-2 text-black text-xl font-bold">AB</span>
@@ -30,7 +29,7 @@ const Navbar = () => {
         <ul className="hidden md:flex items-center gap-4">
           {renderMenuItems()}
         </ul>
-        <button className="bg-blue-600   px-3 py-2 rounded-lg md:hidden" onClick={toggleMenu}>
+        <button className="bg-blue-600 px-3 py-2 rounded-lg md:hidden" onClick={toggleMenu}>
           <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
